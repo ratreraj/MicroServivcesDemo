86c41e7449937dc9c89062c7c23741cc244d5c75
Raj Prakash Ratre <8319633+ratreraj@users.noreply.github.com>
2021-09-21 10:05:29 +0530

    Add project files.

---
+++ UserMicroservice/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserMicroservice.Database;
+using UserMicroservice.Database.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+         DatabaseContext _databaseContext;
+        public UserController( DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _databaseContext.Users.ToList();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UserMicroservice/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserMicroservice.Database.Entities;
+namespace UserMicroservice.Database
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=DESKTOP-HA42MEA\SQLEXPRESS; initial catalog=UserMicroservice;persist security info=True;user id=sa;password=Sql@2019; MultipleActiveResultSets=True;");
+        }
+    }
+}
